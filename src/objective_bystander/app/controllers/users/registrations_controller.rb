70cd18ae9794b73ca089cd066ccb362ed9dc606e
Llausa <santiago.chamon@gmail.com>
2019-11-07 14:45:21 +1000

    roles are assigned on registration

--- src/objective_bystander/app/controllers/users/registrations_controller.rb
+++ src/objective_bystander/app/controllers/users/registrations_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
+  # after_create :set_role
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,9 +11,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    puts params
+    resource.roles << Role.find(params[:user][:role_ids])
+  end
 
   # GET /resource/edit
   # def edit
@@ -38,12 +41,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  #
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- src/objective_bystander/config/routes.rb
+++ src/objective_bystander/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :advice_replies
   resources :advice_questions
   resources :topics
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
